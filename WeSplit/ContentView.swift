c14a9541f97effee239c203ff43b2bc9b073480c
Juan Dorado <fdorado985@gmail.com>
2020-10-02 12:24:24 -0500

    Improve closure using shorthand syntax

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -12,8 +12,8 @@ struct ContentView: View {
 
   var body: some View {
     Form {
-      ForEach(0 ..< 100) { number in
-        Text("Row \(number)")
+      ForEach(0 ..< 100) {
+        Text("Row \($0)")
       }
     }
   }
